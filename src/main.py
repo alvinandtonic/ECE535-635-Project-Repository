ad6868e856a15d78b46cbd4105fe1a628ac4f119
Ligeng Liu <128413622+LeoTom4to@users.noreply.github.com>
2023-10-31 20:38:38 -0400

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import configparser
+import argparse
+import logging
+import os
+import warnings
+import torch
+from mpi4py import MPI
+from fl import FL
+
+
+# For MPI experiments
+COMM = MPI.COMM_WORLD
+RANK = COMM.Get_rank()
+
+
+def main():
+    is_mpi = COMM.Get_size() != 1
+    config = read_config()
+    fl = FL(config, is_mpi, RANK)
+    fl.start()
+
+
+
+def read_config():
+  config = configparser.ConfigParser()
+  config.read('config/opp/dccae/A0_B0_AB30_label_AB_test_B')
+  return config
+config = read_config()
+fl = FL(config)
+fl.start()
+
+
+if __name__ == "__main__":
+    main()
